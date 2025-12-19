93f278fd233521031a69d0577b7ac581c9510c76
Tuan Anh Kieu <tanhls222@gmail.comm>
2025-12-19 11:22:45 +0700

    fix api dang nhap nguoi nha, them api lich chung'

--- controller/authcontroller.js
+++ controller/authcontroller.js
@@ -65,7 +65,7 @@ const login = async (req, res, next) => {
             token,
             user: {
                 ...user,
-                ho_so_nhan_vien: hoSoNhanVien
+                ...hoSoNhanVien
             }
         };
 
