c55be69acadc05eae554b3cdfc2decc5eb19a891
trantonhu <trantonhu04@gmail.com>
2025-12-23 10:44:15 +0700

    fix bug

--- routes/nhanVienRoutes.js
+++ routes/nhanVienRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const nhanVienController = require('../controller/nhanVienController')
 const { authenticate,checkAppPermission } = require('../middlewares/auth');
 
-router.get('/:idBenhNhan', authenticate, nhanVienController.layDanhSachNhanVien);
-router.get('/ds/:idDieuDuong', authenticate,nhanVienController.layChiTietNhanVien);
+router.get('/ds/:idBenhNhan', authenticate, nhanVienController.layDanhSachNhanVien);
+router.get('/thong-tin/:idNhanVien', authenticate,nhanVienController.layChiTietNhanVien)
 
 module.exports = router;
