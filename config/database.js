cda3706982dd3feee13c6a9360223e4295e8fa4c
ntl-anh <126383533+ntl-anh@users.noreply.github.com>
2025-12-26 08:57:06 +0700

    config

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ const connection = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'duong_lao',
+  database: process.env.DB_NAME || 'quanlyduonglao',
   port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
